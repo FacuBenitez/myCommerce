bd38e6ff685a468588c467741efbcda7704cc954
FacuBenitez <facu98ben@gmail.com>
2021-11-18 14:48:22 -0300

    Creando navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './NavbarStyles.scss';
+
+const Navbar = () => {
+    return (
+        <>
+            <nav>
+                <p>myCommerce</p>
+
+                <li>Home</li>
+                <li>Contact us</li>
+                <li>Services</li>
+            </nav>
+
+        </>
+    )
+}
+
+export default Navbar
